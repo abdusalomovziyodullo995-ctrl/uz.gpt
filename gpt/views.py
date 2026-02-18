d29598f1d46fcdf1e021dbc1242a418551631735
Sardorbek <abdusalomovziyodullo995@gmail.com>
2026-02-18 22:48:10 +0500

    1_versiya

--- gpt/views.py
+++ gpt/views.py
@@ -1,26 +1,47 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate
 
-# def Html(request):
 
-#     return render(request,"templates.html")
+# def login_user(request):
+#     if request.method == "POST":
 
+#         email = request.POST.get("email")
+#         password = request.POST.get("password")
+#         user = authenticate(username=email , password=password)
+
+#         if user is not None:
+#             return render(request , "home.html")
+#         else:
+#             return redirect("login_user")
+#             # print("user mavjud emas")
+#     else:
+#         return render(request , "login.html")    
+
+
+
+
+
+
+
+from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 def login_user(request):
-    if request.method == "POST":
+    error_message = ""  # Xato xabar uchun
 
+    if request.method == "POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
 
-        user = authenticate(username=email , password=password)
+        user = authenticate(username=email, password=password)
 
         if user is not None:
-            return render(request , "home.html")
+            return render(request, "home.html")
         else:
-            return redirect("login_user")
-            # print("user mavjud emas")
-    else:
-        return render(request , "login.html")    
+            error_message = "Email yoki parol hato"
+
+    return render(request, "login.html", {"error_message": error_message})
+
 
 
 
